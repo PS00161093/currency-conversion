88f45d94dfa0837939c98a650669bd12660a8a83
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-02 22:28:36 +0530

    Calling currency-exchange service using Feign client

--- src/main/java/com/example/ps/currencyconversion/CurrencyConversionApplication.java
+++ src/main/java/com/example/ps/currencyconversion/CurrencyConversionApplication.java
@@ -2,8 +2,10 @@ package com.example.ps.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/ps/currencyconversion/webclient/CurrencyExchangeClient.java
@@ -0,0 +1,15 @@
+package com.example.ps.currencyconversion.webclient;
+
+import com.example.ps.currencyconversion.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeClient {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+}
